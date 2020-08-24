4ffca3d8361167bc3f4a921bdbdd9c8cfc604374
David O'Rourke <david.orourke@gmail.com>
2020-08-24 01:15:21 +0100

    mode_type: Improve validation

--- src/mode_type.rs
+++ src/mode_type.rs
@@ -6,6 +6,15 @@ use std::str::FromStr;
 const VALID_FILE_MODE: &[char] = &['-', 'r', 'w', 'x', 's', 'S', 't', 'T'];
 const VALID_OCTAL: RangeInclusive<char> = RangeInclusive::new('0', '7');
 
+// Characters allowed in positions that can't contain special characters
+const NORMAL_CHARS: &[char] = &['-', 'r', 'w'];
+
+// Characters allowed in the usr or grp exec fields
+const USRGRP_EXEC_CHARS: &[char] = &['-', 'x', 's', 'S'];
+
+// Characters allowed in the oth exec field
+const OTH_EXEC_CHARS: &[char] = &['-', 'x', 't', 'T'];
+
 #[derive(Debug, PartialEq)]
 pub enum ModeType {
     FileMode,
@@ -16,13 +25,46 @@ impl FromStr for ModeType {
     type Err = Error;
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
+        // Valid input lengths
+        // 3: 755
+        // 4: 1755
+        // 9: rwxrwxrwx
+        if ![3, 4, 9].contains(&input.len()) {
+            return Err(Error::InvalidModeString);
+        }
+
+        // Check for input composed entirely of valid octal characters
         if input.chars().all(|c| VALID_OCTAL.contains(&c)) {
             let mt = Self::OctalNumeric;
 
             return Ok(mt);
         }
 
+        // Check for input composed entirely of valid file mode characters
         if input.chars().all(|c| VALID_FILE_MODE.contains(&c)) {
+            for (i, c) in input.chars().enumerate() {
+                match i {
+                    // Indexes that cannot contain suid, sgid, or sticky bits
+                    0 | 1 | 3 | 4 | 6 | 7 => {
+                        if !NORMAL_CHARS.contains(&c) {
+                            return Err(Error::InvalidModeString);
+                        }
+                    },
+                    // Indexes that could be suid or sgid
+                    2 | 5 => {
+                        if !USRGRP_EXEC_CHARS.contains(&c) {
+                            return Err(Error::InvalidModeString);
+                        }
+                    },
+                    8 => {
+                        if !OTH_EXEC_CHARS.contains(&c) {
+                            return Err(Error::InvalidModeString);
+                        }
+                    },
+                    _ => unreachable!(),
+                }
+            }
+
             let mt = Self::FileMode;
 
             return Ok(mt);
